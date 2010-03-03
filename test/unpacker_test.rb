8ed6073673ed3af94fd864ae93f934ebea34b666
Yuya Kato <yuyakato@gmail.com>
2010-03-03 16:37:54 +0900

    リファクタリング

--- test/unpacker_test.rb
+++ test/unpacker_test.rb
@@ -21,26 +21,26 @@ class UnpackerTest < Test::Unit::TestCase
   end
 
   def test_unpack__uint8
-    assert_equal(0,          unpack("\xCC\x00"))
-    assert_equal(2 ** 8 - 1, unpack("\xCC\xFF"))
+    assert_equal(0,    unpack("\xCC\x00"))
+    assert_equal(0xFF, unpack("\xCC\xFF"))
   end
 
   def test_unpack__uint16
-    assert_equal(0,           unpack("\xCD\x00\x00"))
-    assert_equal(1,           unpack("\xCD\x00\x01"))
-    assert_equal(2 ** 16 - 1, unpack("\xCD\xFF\xFF"))
+    assert_equal(0x0000, unpack("\xCD\x00\x00"))
+    assert_equal(0x0001, unpack("\xCD\x00\x01"))
+    assert_equal(0xFFFF, unpack("\xCD\xFF\xFF"))
   end
 
   def test_unpack__uint32
-    assert_equal(0,           unpack("\xCE\x00\x00\x00\x00"))
-    assert_equal(1,           unpack("\xCE\x00\x00\x00\x01"))
-    assert_equal(2 ** 32 - 1, unpack("\xCE\xFF\xFF\xFF\xFF"))
+    assert_equal(0x00000000, unpack("\xCE\x00\x00\x00\x00"))
+    assert_equal(0x00000001, unpack("\xCE\x00\x00\x00\x01"))
+    assert_equal(0xFFFFFFFF, unpack("\xCE\xFF\xFF\xFF\xFF"))
   end
 
   def test_unpack__uint64
-    assert_equal(0,           unpack("\xCF\x00\x00\x00\x00\x00\x00\x00\x00"))
-    assert_equal(1,           unpack("\xCF\x00\x00\x00\x00\x00\x00\x00\x01"))
-    assert_equal(2 ** 64 - 1, unpack("\xCF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"))
+    assert_equal(0x0000000000000000, unpack("\xCF\x00\x00\x00\x00\x00\x00\x00\x00"))
+    assert_equal(0x0000000000000001, unpack("\xCF\x00\x00\x00\x00\x00\x00\x00\x01"))
+    assert_equal(0xFFFFFFFFFFFFFFFF, unpack("\xCF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"))
   end
 
   def test_unpack__int8
@@ -105,16 +105,16 @@ class UnpackerTest < Test::Unit::TestCase
     assert_equal("",    unpack("\xDA\x00\x00"))
     assert_equal("ABC", unpack("\xDA\x00\x03ABC"))
     assert_equal(
-      "A" * (2 ** 16 - 1),
-      unpack("\xDA\xFF\xFF" + "A" * (2 ** 16 - 1)))
+      "A" * 0xFFFF,
+      unpack("\xDA\xFF\xFF" + "A" * 0xFFFF))
   end
 
   def test_unpack__raw32
     assert_equal("",    unpack("\xDB\x00\x00\x00\x00"))
     assert_equal("ABC", unpack("\xDB\x00\x00\x00\x03ABC"))
     assert_equal(
-      "A" * (2 ** 16),
-      unpack("\xDB\x00\x01\x00\x00" + "A" * (2 ** 16)))
+      "A" * 0x10000,
+      unpack("\xDB\x00\x01\x00\x00" + "A" * 0x10000))
   end
 
   def test_unpack__fixarray
@@ -127,16 +127,16 @@ class UnpackerTest < Test::Unit::TestCase
     assert_equal([],        unpack("\xDC\x00\x00"))
     assert_equal([0, 1, 2], unpack("\xDC\x00\x03\x00\x01\x02"))
     assert_equal(
-      [0] * (2 ** 16 - 1),
-      unpack("\xDC\xFF\xFF" + "\x00" * (2 ** 16 - 1)))
+      [0] * 0xFFFF,
+      unpack("\xDC\xFF\xFF" + "\x00" * 0xFFFF))
   end
 
   def test_unpack__array32
     assert_equal([],        unpack("\xDD\x00\x00\x00\x00"))
     assert_equal([0, 1, 2], unpack("\xDD\x00\x00\x00\x03\x00\x01\x02"))
     assert_equal(
-      [0] * (2 ** 16),
-      unpack("\xDD\x00\x01\x00\x00" + "\x00" * (2 ** 16)))
+      [0] * 0x10000,
+      unpack("\xDD\x00\x01\x00\x00" + "\x00" * 0x10000))
   end
 
   def test_unpack__fixmap
@@ -155,7 +155,7 @@ class UnpackerTest < Test::Unit::TestCase
     hash = {}
     io   = StringIO.new
     io.write("\xDE\xFF\xFF")
-    (2 ** 16 - 1).times { |i|
+    0xFFFF.times { |i|
       hash[i] = 0
       io.write("\xCD") # uint16: i
       io.write([i].pack("n"))
@@ -174,7 +174,7 @@ class UnpackerTest < Test::Unit::TestCase
     hash = {}
     io   = StringIO.new
     io.write("\xDF\x00\x01\x00\x00")
-    (2 ** 16).times { |i|
+    0x10000.times { |i|
       hash[i] = 0
       io.write("\xCD") # uint16: i
       io.write([i].pack("n"))

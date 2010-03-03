5de4a734cb26ac6f05b483b540a5440279bc26f6
Yuya Kato <yuyakato@gmail.com>
2010-03-03 11:56:09 +0900

    negative fixnum、uint8を実装

--- lib/msgpack_pure/unpacker.rb
+++ lib/msgpack_pure/unpacker.rb
@@ -13,6 +13,12 @@ module MessagePackPure::Unpacker
 
     if (type & 0b10000000) == 0b00000000 # positive fixnum
       return type
+    elsif (type & 0b11100000) == 0b11100000 # negative fixnum
+      return (type & 0b00011111) - 32
+    end
+
+    if type == 0xCC
+      return io.read(1).unpack("C")[0]
     end
 
     return nil
--- test/unpacker_test.rb
+++ test/unpacker_test.rb
@@ -11,9 +11,19 @@ class UnpackerTest < Test::Unit::TestCase
 
   def test_positive_fixnum
     assert_equal(  0, @module.unpack(StringIO.new("\x00")))
-    assert_equal(  1, @module.unpack(StringIO.new("\x01")))
     assert_equal(127, @module.unpack(StringIO.new("\x7F")))
   end
+
+  def test_negative_fixnum
+    assert_equal( -1, @module.unpack(StringIO.new("\xFF")))
+    assert_equal(-32, @module.unpack(StringIO.new("\xE0")))
+  end
+
+  def test_unit8
+    assert_equal(128,        @module.unpack(StringIO.new("\xCC\x80")))
+    assert_equal(2 ** 8 - 1, @module.unpack(StringIO.new("\xCC\xFF")))
+  end
+
 
   def test_nil
     io = StringIO.new("\xC0")

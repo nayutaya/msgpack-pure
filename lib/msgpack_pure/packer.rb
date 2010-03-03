ced699c6f58aba4278ac467b8e784483214cee56
Yuya Kato <yuyakato@gmail.com>
2010-03-03 17:14:32 +0900

    シリアライザの雛形を追加

---
+++ lib/msgpack_pure/packer.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# MessagePack format specification
+# http://msgpack.sourceforge.jp/spec
+
+module MessagePackPure
+  module Packer
+  end
+end
+
+module MessagePackPure::Packer
+end
---
+++ test/packer_test.rb
@@ -0,0 +1,16 @@
+#! ruby -Ku
+# coding: utf-8
+
+require "test_helper"
+require "stringio"
+require "msgpack_pure/packer"
+
+class PackerTest < Test::Unit::TestCase
+  def setup
+    @module = MessagePackPure::Packer
+  end
+
+  def test_pack
+    assert true
+  end
+end

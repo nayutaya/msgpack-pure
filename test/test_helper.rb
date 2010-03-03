53cbee9ba3bd35e119754462f949d2720b073ff8
Yuya Kato <yuyakato@gmail.com>
2010-03-03 11:45:08 +0900

    redgreen、win32consoleのロードを追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,11 @@
 $:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
 require "test/unit"
+
+begin
+  require "rubygems"
+  require "redgreen"
+  require "win32console" if /win32/ =~ RUBY_PLATFORM
+rescue LoadError
+  # nop
+end

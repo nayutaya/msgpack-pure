130b59fcc5954194a374c3bbcd8e46b6acf674e4
Yuya Kato <yuyakato@gmail.com>
2010-03-03 15:12:50 +0900

    テストケースを整理

--- test/unpacker_test.rb
+++ test/unpacker_test.rb
@@ -20,28 +20,33 @@ class UnpackerTest < Test::Unit::TestCase
   end
 
   def test_uint8
-    assert_equal(128,        @module.unpack(StringIO.new("\xCC\x80")))
+    assert_equal(0,          @module.unpack(StringIO.new("\xCC\x00")))
     assert_equal(2 ** 8 - 1, @module.unpack(StringIO.new("\xCC\xFF")))
   end
 
   def test_uint16
-    assert_equal(2 ** 8,      @module.unpack(StringIO.new("\xCD\x01\x00")))
+    assert_equal(0,           @module.unpack(StringIO.new("\xCD\x00\x00")))
+    assert_equal(1,           @module.unpack(StringIO.new("\xCD\x00\x01")))
     assert_equal(2 ** 16 - 1, @module.unpack(StringIO.new("\xCD\xFF\xFF")))
   end
 
   def test_uint32
-    assert_equal(2 ** 16,     @module.unpack(StringIO.new("\xCE\x00\x01\x00\x00")))
+    assert_equal(0,           @module.unpack(StringIO.new("\xCE\x00\x00\x00\x00")))
+    assert_equal(1,           @module.unpack(StringIO.new("\xCE\x00\x00\x00\x01")))
     assert_equal(2 ** 32 - 1, @module.unpack(StringIO.new("\xCE\xFF\xFF\xFF\xFF")))
   end
 
   def test_uint64
-    assert_equal(2 ** 32,     @module.unpack(StringIO.new("\xCF\x00\x00\x00\x01\x00\x00\x00\x00")))
+    assert_equal(0,           @module.unpack(StringIO.new("\xCF\x00\x00\x00\x00\x00\x00\x00\x00")))
+    assert_equal(1,           @module.unpack(StringIO.new("\xCF\x00\x00\x00\x00\x00\x00\x00\x01")))
     assert_equal(2 ** 64 - 1, @module.unpack(StringIO.new("\xCF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF")))
   end
 
   def test_int8
-    assert_equal(-33,  @module.unpack(StringIO.new("\xD0\xDF")))
-    assert_equal(-128, @module.unpack(StringIO.new("\xD0\x80")))
+    assert_equal(0,          @module.unpack(StringIO.new("\xD0\x00")))
+    assert_equal(2 ** 7 - 1, @module.unpack(StringIO.new("\xD0\x7F")))
+    assert_equal(-1,         @module.unpack(StringIO.new("\xD0\xFF")))
+    assert_equal(-(2 ** 7),  @module.unpack(StringIO.new("\xD0\x80")))
   end
 
   def test_int16
@@ -84,6 +89,7 @@ class UnpackerTest < Test::Unit::TestCase
   end
 
   def test_double
+    assert_equal(0.0,  @module.unpack(StringIO.new("\xCB\x00\x00\x00\x00\x00\x00\x00\x00")))
     assert_equal(0.5,  @module.unpack(StringIO.new("\xCB\x3F\xE0\x00\x00\x00\x00\x00\x00")))
     assert_equal(-0.5, @module.unpack(StringIO.new("\xCB\xBF\xE0\x00\x00\x00\x00\x00\x00")))
   end

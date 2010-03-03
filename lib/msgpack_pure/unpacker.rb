0fc6ccd754ca22688d3e5298393f9b7e7b76a6fe
Yuya Kato <yuyakato@gmail.com>
2010-03-03 11:50:19 +0900

    nil、positive fixnumを実装

--- lib/msgpack_pure/unpacker.rb
+++ lib/msgpack_pure/unpacker.rb
@@ -1,9 +1,20 @@
 # coding: utf-8
 
+require "stringio"
+
 module MessagePackPure
   module Unpacker
   end
 end
 
 module MessagePackPure::Unpacker
+  def self.unpack(io)
+    type = io.read(1).unpack("C")[0]
+
+    if (type & 0b10000000) == 0b00000000 # positive fixnum
+      return type
+    end
+
+    return nil
+  end
 end
--- test/unpacker_test.rb
+++ test/unpacker_test.rb
@@ -5,6 +5,21 @@ require "test_helper"
 require "msgpack_pure/unpacker"
 
 class UnpackerTest < Test::Unit::TestCase
+  def setup
+    @module = MessagePackPure::Unpacker
+  end
+
+  def test_positive_fixnum
+    assert_equal(  0, @module.unpack(StringIO.new("\x00")))
+    assert_equal(  1, @module.unpack(StringIO.new("\x01")))
+    assert_equal(127, @module.unpack(StringIO.new("\x7F")))
+  end
+
+  def test_nil
+    io = StringIO.new("\xC0")
+    assert_equal(nil, @module.unpack(io))
+  end
+
   def test_ok
     assert true
   end

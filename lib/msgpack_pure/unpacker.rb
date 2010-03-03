dcd22fee1b46c0f906ef47445083f32316c1a510
Yuya Kato <yuyakato@gmail.com>
2010-03-03 14:22:32 +0900

    uint16、uint32を実装

--- lib/msgpack_pure/unpacker.rb
+++ lib/msgpack_pure/unpacker.rb
@@ -17,8 +17,15 @@ module MessagePackPure::Unpacker
       return (type & 0b00011111) - 32
     end
 
-    if type == 0xCC
+    if type == 0xCC # uint8
       return io.read(1).unpack("C")[0]
+    elsif type == 0xCD # uint16
+      return io.read(2).unpack("n")[0]
+    end
+
+    case type
+    when 0xCE # uint32
+      return io.read(4).unpack("N")[0]
     end
 
     return nil
--- test/unpacker_test.rb
+++ test/unpacker_test.rb
@@ -24,6 +24,15 @@ class UnpackerTest < Test::Unit::TestCase
     assert_equal(2 ** 8 - 1, @module.unpack(StringIO.new("\xCC\xFF")))
   end
 
+  def test_unit16
+    assert_equal(2 ** 8,      @module.unpack(StringIO.new("\xCD\x01\x00")))
+    assert_equal(2 ** 16 - 1, @module.unpack(StringIO.new("\xCD\xFF\xFF")))
+  end
+
+  def test_unit32
+    assert_equal(2 ** 16,     @module.unpack(StringIO.new("\xCE\x00\x01\x00\x00")))
+    assert_equal(2 ** 32 - 1, @module.unpack(StringIO.new("\xCE\xFF\xFF\xFF\xFF")))
+  end
 
   def test_nil
     io = StringIO.new("\xC0")

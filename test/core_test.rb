32cce607f149a80f60e98978d0f088662c884a46
Yuya Kato <yuyakato@gmail.com>
2010-03-03 19:14:01 +0900

    テストケースを追加

--- test/core_test.rb
+++ test/core_test.rb
@@ -11,9 +11,11 @@ class CoreTest < Test::Unit::TestCase
 
   def test_pack
     assert_equal("\x00", @module.pack(0))
+    assert_equal("\xC0", @module.pack(nil))
   end
 
   def test_unpack
-    assert_equal(0, @module.unpack("\x00"))
+    assert_equal(0,   @module.unpack("\x00"))
+    assert_equal(nil, @module.unpack("\xC0"))
   end
 end

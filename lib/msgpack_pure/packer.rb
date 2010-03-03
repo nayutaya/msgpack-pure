223bb8ec838d222a68d7acb349dd420803eaaeb8
Yuya Kato <yuyakato@gmail.com>
2010-03-03 17:18:22 +0900

    true、falseを実装

--- lib/msgpack_pure/packer.rb
+++ lib/msgpack_pure/packer.rb
@@ -13,4 +13,14 @@ module MessagePackPure::Packer
     io.write("\xC0")
     return io
   end
+
+  def self.pack_true(io)
+    io.write("\xC3")
+    return io
+  end
+
+  def self.pack_false(io)
+    io.write("\xC2")
+    return io
+  end
 end
--- test/packer_test.rb
+++ test/packer_test.rb
@@ -14,4 +14,12 @@ class PackerTest < Test::Unit::TestCase
   def test_pack_nil
     assert_equal("\xC0", @module.pack_nil(@io).string)
   end
+
+  def test_pack_true
+    assert_equal("\xC3", @module.pack_true(@io).string)
+  end
+
+  def test_pack_false
+    assert_equal("\xC2", @module.pack_false(@io).string)
+  end
 end

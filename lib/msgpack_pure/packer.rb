66b5ed6d67a35b13523d9d7b881868b3e91359ed
Yuya Kato <yuyakato@gmail.com>
2010-03-03 18:43:44 +0900

    リファクタリング

--- lib/msgpack_pure/packer.rb
+++ lib/msgpack_pure/packer.rb
@@ -40,36 +40,35 @@ module MessagePackPure::Packer
     when (0x0000..0xFFFF)
       # uint16
       io.write("\xCD")
-      io.write([num].pack("n"))
+      io.write(self.pack_uint16(num))
     when (-0x8000..0x7FFF)
       # int16
       io.write("\xD1")
-      num += (2 ** 16) if num < 0
-      io.write([num].pack("n"))
+      io.write(self.pack_int16(num))
     when (0x00000000..0xFFFFFFFF)
       # uint32
       io.write("\xCE")
-      io.write([num].pack("N"))
+      io.write(self.pack_uint32(num))
     when (-0x80000000..0x7FFFFFFF)
       # int32
       io.write("\xD2")
       num += (2 ** 32) if num < 0
-      io.write([num].pack("N"))
+      io.write(self.pack_uint32(num))
     when (0x0000000000000000..0xFFFFFFFFFFFFFFFF)
       # uint64
       high = (num >> 32)
       low  = (num & 0xFFFFFFFF)
       io.write("\xCF")
-      io.write([high].pack("N"))
-      io.write([low].pack("N"))
+      io.write(self.pack_uint32(high))
+      io.write(self.pack_uint32(low))
     when (-0x8000000000000000..0x7FFFFFFFFFFFFFFF)
       # int64
       num += (2 ** 64) if num < 0
       high = (num >> 32)
       low  = (num & 0xFFFFFFFF)
       io.write("\xD3")
-      io.write([high].pack("N"))
-      io.write([low].pack("N"))
+      io.write(self.pack_uint32(high))
+      io.write(self.pack_uint32(low))
     else
       raise("invalid integer")
     end
@@ -96,17 +95,17 @@ module MessagePackPure::Packer
     case value.size
     when (0x00..0x1F)
       # fixraw
-      io.write([0b10100000 + value.size].pack("C"))
+      io.write(self.pack_uint8(0b10100000 + value.size))
       io.write(value)
     when (0x0000..0xFFFF)
       # raw16
       io.write("\xDA")
-      io.write([value.size].pack("n"))
+      io.write(self.pack_uint16(value.size))
       io.write(value)
     when (0x00000000..0xFFFFFFFF)
       # raw32
       io.write("\xDB")
-      io.write([value.size].pack("N"))
+      io.write(self.pack_uint32(value.size))
       io.write(value)
     else
       raise("invalid length")
@@ -121,11 +120,11 @@ module MessagePackPure::Packer
     when (0x0000..0xFFFF)
       # array16
       io.write("\xDC")
-      io.write([value.size].pack("n"))
+      io.write(self.pack_uint16(value.size))
     when (0x00000000..0xFFFFFFFF)
       # array32
       io.write("\xDD")
-      io.write([value.size].pack("N"))
+      io.write(self.pack_uint32(value.size))
     else
       raise("invalid length")
     end
@@ -143,11 +142,11 @@ module MessagePackPure::Packer
     when (0x0000..0xFFFF)
       # map16
       io.write("\xDE")
-      io.write([value.size].pack("n"))
+      io.write(self.pack_uint16(value.size))
     when (0x00000000..0xFFFFFFFF)
       # map32
       io.write("\xDF")
-      io.write([value.size].pack("N"))
+      io.write(self.pack_uint32(value.size))
     else
       raise("invalid length")
     end
@@ -164,5 +163,18 @@ module MessagePackPure::Packer
 
   def self.pack_int8(value)
     return [value].pack("c")
+  end
+
+  def self.pack_uint16(value)
+    return [value].pack("n")
+  end
+
+  def self.pack_int16(value)
+    value += (2 ** 16) if value < 0
+    return self.pack_uint16(value)
+  end
+
+  def self.pack_uint32(value)
+    return [value].pack("N")
   end
 end

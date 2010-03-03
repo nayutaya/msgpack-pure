5bf6bf07df06698940e2be22a6c16d7e1ca66aa7
Yuya Kato <yuyakato@gmail.com>
2010-03-03 17:29:56 +0900

    リファクタリング

--- test/packer_test.rb
+++ test/packer_test.rb
@@ -10,28 +10,28 @@ class PackerTest < Test::Unit::TestCase
     @module = MessagePackPure::Packer
   end
 
-  def test_fixnum__positive_fixnum
+  def test_pack_fixnum__positive_fixnum
     assert_equal("\x00", @module.pack_fixnum(sio, 0x00).string)
     assert_equal("\x01", @module.pack_fixnum(sio, 0x01).string)
     assert_equal("\x7F", @module.pack_fixnum(sio, 0x7F).string)
   end
 
-  def test_fixnum__negative_fixnum
+  def test_pack_fixnum__negative_fixnum
     assert_equal("\xFF", @module.pack_fixnum(sio, -0x01).string)
     assert_equal("\xE0", @module.pack_fixnum(sio, -0x20).string)
   end
 
-  def test_fixnum__uint8
+  def test_pack_fixnum__uint8
     assert_equal("\xCC\x80", @module.pack_fixnum(sio, 0x80).string)
     assert_equal("\xCC\xFF", @module.pack_fixnum(sio, 0xFF).string)
   end
 
-  def test_fixnum__uint16
+  def test_pack_fixnum__uint16
     assert_equal("\xCD\x01\x00", @module.pack_fixnum(sio, 0x0100).string)
     assert_equal("\xCD\xFF\xFF", @module.pack_fixnum(sio, 0xFFFF).string)
   end
 
-  def test_fixnum__uint32
+  def test_pack_fixnum__uint32
     assert_equal("\xCE\x00\x01\x00\x00", @module.pack_fixnum(sio, 0x00010000).string)
     assert_equal("\xCE\xFF\xFF\xFF\xFF", @module.pack_fixnum(sio, 0xFFFFFFFF).string)
   end

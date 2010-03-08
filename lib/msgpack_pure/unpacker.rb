97ae63d2381a70a13e63b32aa95506a960899a86
Yuya Kato <yuyakato@gmail.com>
2010-03-08 11:52:19 +0900

    クラスメソッドをインスタンスメソッドに変更

--- lib/msgpack_pure/unpacker.rb
+++ lib/msgpack_pure/unpacker.rb
@@ -15,12 +15,13 @@ class MessagePackPure::Unpacker
 
   attr_reader :io
 
-  def read
-    return self.class.read(@io)
+  def self.read(io)
+    unpacker = self.new(io)
+    return unpacker.read
   end
 
-  def self.read(io)
-    type = self.unpack_uint8(io)
+  def read
+    type = self.unpack_uint8
 
     case
     when (type & 0b10000000) == 0b00000000 # positive fixnum
@@ -29,13 +30,13 @@ class MessagePackPure::Unpacker
       return (type & 0b00011111) - (2 ** 5)
     when (type & 0b11100000) == 0b10100000 # fixraw
       size = (type & 0b00011111)
-      return io.read(size)
+      return @io.read(size)
     when (type & 0b11110000) == 0b10010000 # fixarray
       size = (type & 0b00001111)
-      return self.read_array(io, size)
+      return self.read_array(size)
     when (type & 0b11110000) == 0b10000000 # fixmap
       size = (type & 0b00001111)
-      return self.read_hash(io, size)
+      return self.read_hash(size)
     end
 
     case type
@@ -46,101 +47,101 @@ class MessagePackPure::Unpacker
     when 0xC3 # true
       return true
     when 0xCA # float
-      return self.unpack_float(io)
+      return self.unpack_float
     when 0xCB # double
-      return self.unpack_double(io)
+      return self.unpack_double
     when 0xCC # uint8
-      return self.unpack_uint8(io)
+      return self.unpack_uint8
     when 0xCD # uint16
-      return self.unpack_uint16(io)
+      return self.unpack_uint16
     when 0xCE # uint32
-      return self.unpack_uint32(io)
+      return self.unpack_uint32
     when 0xCF # uint64
-      return self.unpack_uint64(io)
+      return self.unpack_uint64
     when 0xD0 # int8
-      return self.unpack_int8(io)
+      return self.unpack_int8
     when 0xD1 # int16
-      return self.unpack_int16(io)
+      return self.unpack_int16
     when 0xD2 # int32
-      return self.unpack_int32(io)
+      return self.unpack_int32
     when 0xD3 # int64
-      return self.unpack_int64(io)
+      return self.unpack_int64
     when 0xDA # raw16
-      size = self.unpack_uint16(io)
-      return io.read(size)
+      size = self.unpack_uint16
+      return @io.read(size)
     when 0xDB # raw32
-      size = self.unpack_uint32(io)
-      return io.read(size)
+      size = self.unpack_uint32
+      return @io.read(size)
     when 0xDC # array16
-      size = self.unpack_uint16(io)
-      return self.read_array(io, size)
+      size = self.unpack_uint16
+      return self.read_array(size)
     when 0xDD # array32
-      size = self.unpack_uint32(io)
-      return self.read_array(io, size)
+      size = self.unpack_uint32
+      return self.read_array(size)
     when 0xDE # map16
-      size = self.unpack_uint16(io)
-      return self.read_hash(io, size)
+      size = self.unpack_uint16
+      return self.read_hash(size)
     when 0xDF # map32
-      size = self.unpack_uint32(io)
-      return self.read_hash(io, size)
+      size = self.unpack_uint32
+      return self.read_hash(size)
     else
       raise("Unknown Type -- #{'0x%02X' % type}")
     end
   end
 
-  def self.read_array(io, size)
-    return size.times.map { self.read(io) }
+  def read_array(size)
+    return size.times.map { self.read }
   end
 
-  def self.read_hash(io, size)
+  def read_hash(size)
     return size.times.inject({}) { |memo,|
-      memo[self.read(io)] = self.read(io)
+      memo[self.read] = self.read
       memo
     }
   end
 
-  def self.unpack_uint8(io)
-    return io.read(1).unpack("C")[0]
+  def unpack_uint8
+    return @io.read(1).unpack("C")[0]
   end
 
-  def self.unpack_int8(io)
-    return io.read(1).unpack("c")[0]
+  def unpack_int8
+    return @io.read(1).unpack("c")[0]
   end
 
-  def self.unpack_uint16(io)
-    return io.read(2).unpack("n")[0]
+  def unpack_uint16
+    return @io.read(2).unpack("n")[0]
   end
 
-  def self.unpack_int16(io)
-    num = self.unpack_uint16(io)
+  def unpack_int16
+    num = self.unpack_uint16
     return (num < 2 ** 15 ? num : num - (2 ** 16))
   end
 
-  def self.unpack_uint32(io)
-    return io.read(4).unpack("N")[0]
+  def unpack_uint32
+    return @io.read(4).unpack("N")[0]
   end
 
-  def self.unpack_int32(io)
-    num = self.unpack_uint32(io)
+  def unpack_int32
+    num = self.unpack_uint32
     return (num < 2 ** 31 ? num : num - (2 ** 32))
   end
 
-  def self.unpack_uint64(io)
-    high = self.unpack_uint32(io)
-    low  = self.unpack_uint32(io)
+  def unpack_uint64
+    high = self.unpack_uint32
+    low  = self.unpack_uint32
     return (high << 32) + low
   end
 
-  def self.unpack_int64(io)
-    num = self.unpack_uint64(io)
+  def unpack_int64
+    num = self.unpack_uint64
     return (num < 2 ** 63 ? num : num - (2 ** 64))
   end
 
-  def self.unpack_float(io)
-    return io.read(4).unpack("g")[0]
+  def unpack_float
+    return @io.read(4).unpack("g")[0]
   end
 
-  def self.unpack_double(io)
-    return io.read(8).unpack("G")[0]
+  def unpack_double
+    return @io.read(8).unpack("G")[0]
   end
 end
